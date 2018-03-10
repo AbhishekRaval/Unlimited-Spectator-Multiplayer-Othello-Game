a9cb9dd7bf07f0a271b644a66632386b496f9a1a
abhishek <abhishek.raval@gmail.com>
2018-03-10 18:27:13 -0500

    Basic Setup

--- othello/assets/js/app.js
+++ othello/assets/js/app.js
@@ -18,4 +18,14 @@ import "phoenix_html"
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
-// import socket from "./socket"
+import socket from "./socket"
+
+import run_game from "./game";
+
+function init() {
+  let root = document.getElementById('gamename');
+  run_game(root);
+}
+
+// Use jQuery to delay until page loaded.
+$(init);
