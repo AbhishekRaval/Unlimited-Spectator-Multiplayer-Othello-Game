257de574c49d5bf4b800b9b816ccc5ba6472ce1e
Ketan <kaleketan07@gmail.com>
2018-03-16 22:02:13 -0400

    game logic commit 2

--- othello/lib/othello/game.ex
+++ othello/lib/othello/game.ex
@@ -23,17 +23,80 @@ defmodule othello.Game do
       }
   end
 
-  def handleTileClick (game, row, column) do
+  def handleTileClick (game, row, column, pn) do
 
     clickedTile = game.grid[row][column]
-    if clickedTile === 0 do  # if current user is on of the players TODO
-      newGameState = checkHit(game, clickedTile, row, column)
+    if clickedTile === 0 and isValid(game, row, column)do  # if current user is on of the players TODO
+      if pn === game.p1 or pn === game.p2 do
+          newGameState = checkHit(game, clickedTile, row, column)
+      end
     else
       newGameState = game
     end
     newGameState
 
   end
+
+  def isValid(game, row, column) do
+
+    checkUp(game, row, column)
+    or checkDown(game, row, column)
+    or checkLeft(game, row, column)
+    or checkRight(game, row, column)
+    or checkLeftUp(game, row, column)
+    or checkRightUp(game, row, column)
+    or checkLeftDown(game, row, column)
+    or checkRightDown(game, row, column)
+
+  end
+
+  def checkRightUp() do
+
+  end
+
+  def checkRightDown() do
+
+  end
+
+  def checkLeftUp() do
+
+  end
+
+  def checkLeftDown() do
+
+  end
+
+  def checkRight() do
+
+  end
+
+  def checkLeft() do
+
+  end
+
+  def checkUp() do
+
+  end
+
+  def checkDown() do
+
+  end
+
+
+  ##
+# cases:
+# if diagonal-right-up block has diff value
+# if diagonal-left-up block has diff value
+# if diagonal-right-down block has diff value
+# if diagonal-left-down block has diff value
+#
+
+  ##
+
+  def checkHit(game, clickedTile, row, column) do
+
+  end
+
 
 
 end
